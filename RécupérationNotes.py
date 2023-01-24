3f9bdf97567ce78319026f7f6d551c3edf2bbdae
mathieuaubert2601 <93790696+mathieuaubert2601@users.noreply.github.com>
2023-01-24 15:55:56 +0100

    V1.2

--- RécupérationNotes.py
+++ RécupérationNotes.py
@@ -4,164 +4,182 @@ import requests
 from bs4 import BeautifulSoup
 #Importation de la bibliothèque pour les tableau 
 import numpy as np
-
+#Importation bibliothèque pour utiliser les pattern
 import re
 
-#Récupération des informations de la page
 url ='https://campusonline.inseec.net/note/note_ajax.php?AccountName=B3yyZbaV9FoX0Zh%2BznXga8elZn4c55cGnqCMe2EDACw%3D+&c=classique&mode_affichage=&version=PROD&mode_test=N'
-response = requests.get(url)
 
-#Test de récupération des données 
-if response.status_code == 200:
-    html_content = response.text
-else:
-    print(f'Erreur: {response.status_code}')
-   
-#création de l'objet BeautifulSoup
-soup = BeautifulSoup(html_content, 'html.parser')
+def remplir_Tableau_Info(url_notes) : 
+    #Récupération des informations de la page
+    response = requests.get(url)
+    
+    #Test de récupération des données 
+    if response.status_code == 200:
+        html_content = response.text
+    else:
+        print(f'Erreur: {response.status_code}')
+       
+    #création de l'objet BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+        
+    #récupération des informations des matières 
+    information_gen = soup.find_all('tr', {'class': 'slave master-1'})
+    
+    #creation d'un tableau à trois dimension
+    information_finale = np.empty((8, 15, 30), dtype=np.dtype('U100'))
+    i = 0
+    j=1
+    pourcentage = ""
+    #itère à travers les cellules pour récupérer les Modules et les notes 
+    for cell in information_gen:
+        text = cell.text
+        cellule_separee = text.split()
+        # récupère la cellule qui contient l'information
+        if "Module" in cell.text:
+            information_finale[i,0,0] = text
+            i = i+1
+            j = 1
+        if cell.text.find("Module") == -1 and cell.text.find("Continu") == -1 and cell.text.find("Examen") == -1 and cell.text.find("Semestre") == -1 and cell.text.find("Projet") == -1 or ("Project" in cell.text and cell.text.find("Module") == -1 and cell.text.find("Continu") == -1 and cell.text.find("Examen") == -1 and cell.text.find("Semestre") == -1):
+            if i>0:
+                derniere_chaine_cellule = cellule_separee[-1]
+                derniere_lettre_cellule = derniere_chaine_cellule[-1]
+                information_finale[i-1,j,0]=text
+                information_finale[i-1,j,1]=derniere_lettre_cellule
+                j = j + 1
+        if "Continu" in cell.text:
+            for cellule in cellule_separee:
+                if "%" in cellule:
+                    for elmnt in cellule:
+                        if (elmnt.isdigit() or elmnt == '.'):
+                            pourcentage = pourcentage + elmnt
+                        if(elmnt == "%"):
+                            break
+                    break
+            information_finale[i-1,j-1,2]=pourcentage
+            pourcentage = ""
+        if "Examen" in cell.text:
+             for cellule in cellule_separee:
+                 if "%" in cellule:
+                     for elmnt in cellule:
+                         if (elmnt.isdigit() or elmnt == '.'):
+                             pourcentage = pourcentage + elmnt
+                         if(elmnt == "%"):
+                             break
+                     break
+                         
+             information_finale[i-1,j-1,3]=pourcentage
+             pourcentage = ""
+        if "Projet" in cell.text:
+             for cellule in cellule_separee:
+                 if "%" in cellule:
+                     for elmnt in cellule:
+                         if (elmnt.isdigit() or elmnt == '.'):
+                             pourcentage = pourcentage + elmnt
+                         if(elmnt == "%"):
+                             break
+                     break
+                         
+             information_finale[i-1,j-1,4]=pourcentage
+             pourcentage = ""
+    
+    i = 0
+    j = 0
+    continu = 0
+    examen = 0
+    projet = 0
+    last = 0
+    for tr in soup.find_all('tr', class_='slave master-1'):
+        libelle_list = tr.find_all('td', class_='libelle item-ev1')
+        note_list = tr.find_all('td', class_='note item-ev1')
+        if libelle_list and note_list:
+            libelle = libelle_list[0].text
+            note = note_list[0].text
+            if libelle[0] == 'C':
+                note_sep = note.split()
+                j = j+1 
+                if (information_finale[i][j][0] == '' or information_finale[i][j][0] == ' ') and i<3:
+                    i = i+1
+                    j = 1
+                for elt in note_sep:
+                    if(elt != '-'):
+                        information_finale[i,j,5+continu]=elt
+                        continu = continu + 1
+                continu = 0
+            elif libelle[0] == 'E':
+                note_sep = note.split()
+                j = j+1
+                if(last == 'C' or last == 0):
+                    i = i
+                    j = j - 1
+                elif (information_finale[i][j][0] == '' or information_finale[i][j][0] == ' ') and i<3:
+                    i = i+1
+                    j = 1
+                for elt in note_sep:
+                    if(elt != '-'):
+                        information_finale[i,j,13+examen] = elt
+                        examen = examen + 1
+                examen = 0
+            elif libelle[0] == 'P':
+                note_sep = note.split()
+                j = j+1
+                if(last == 'E' or last == 0):
+                    i = i
+                    j = j - 1
+                elif (information_finale[i][j][0] == '' or information_finale[i][j][0] == ' ') and i<3:
+                    i = i+1
+                    j = 1
+                for elt in note_sep:
+                    if(elt != '-'):
+                        information_finale[i,j,24+projet] = elt
+                        projet = projet + 1
+                projet = 0
+            last = libelle[0]
+    return information_finale
 
-#sélection des informations sur l'année et le semestre le plus récent
-#recuperation de l'entete 
-entete = soup.select_one('td',class_='libelle item-ens')
-text_entete = entete.text
-text_entete_separe = text_entete.split()
-
-#recuperation année et date 
-annee = annee = text_entete_separe[0] + ' ' + text_entete_separe[1]
-date = text_entete_separe[3]
-
-#récupération des informations des matières 
-information_gen = soup.find_all('tr', {'class': 'slave master-1'})
-
-#creation d'un tableau à trois dimension
-information_finale = np.empty((8, 15, 30), dtype=np.dtype('U100'))
-i = 0
-j=1
-pourcentage = ""
-#itère à travers les cellules pour récupérer les Modules et les notes 
-for cell in information_gen:
-    text = cell.text
-    cellule_separee = text.split()
-    # récupère la cellule qui contient l'information
-    if "Module" in cell.text:
-        information_finale[i,0,0] = text
-        i = i+1
-        j = 1
-    if cell.text.find("Module") == -1 and cell.text.find("Continu") == -1 and cell.text.find("Examen") == -1 and cell.text.find("Semestre") == -1 and cell.text.find("Projet") == -1 or ("Project" in cell.text and cell.text.find("Module") == -1 and cell.text.find("Continu") == -1 and cell.text.find("Examen") == -1 and cell.text.find("Semestre") == -1):
-        if i>0:
-            derniere_chaine_cellule = cellule_separee[-1]
-            derniere_lettre_cellule = derniere_chaine_cellule[-1]
-            information_finale[i-1,j,0]=text
-            information_finale[i-1,j,1]=derniere_lettre_cellule
-            j = j + 1
-    if "Continu" in cell.text:
-        for cellule in cellule_separee:
-            if "%" in cellule:
-                for elmnt in cellule:
-                    if (elmnt.isdigit() or elmnt == '.'):
-                        pourcentage = pourcentage + elmnt
-                    if(elmnt == "%"):
-                        break
-                break
-        information_finale[i-1,j-1,2]=pourcentage
-        pourcentage = ""
-    if "Examen" in cell.text:
-         for cellule in cellule_separee:
-             if "%" in cellule:
-                 for elmnt in cellule:
-                     if (elmnt.isdigit() or elmnt == '.'):
-                         pourcentage = pourcentage + elmnt
-                     if(elmnt == "%"):
-                         break
-                 break
-                     
-         information_finale[i-1,j-1,3]=pourcentage
-         pourcentage = ""
-    if "Projet" in cell.text:
-         for cellule in cellule_separee:
-             if "%" in cellule:
-                 for elmnt in cellule:
-                     if (elmnt.isdigit() or elmnt == '.'):
-                         pourcentage = pourcentage + elmnt
-                     if(elmnt == "%"):
-                         break
-                 break
-                     
-         information_finale[i-1,j-1,4]=pourcentage
-         pourcentage = ""
-
-i = 0
-j = 0
-continu = 0
-examen = 0
-projet = 0
-last = 0
-for tr in soup.find_all('tr', class_='slave master-1'):
-    libelle_list = tr.find_all('td', class_='libelle item-ev1')
-    note_list = tr.find_all('td', class_='note item-ev1')
-    if libelle_list and note_list:
-        libelle = libelle_list[0].text
-        note = note_list[0].text
-        if libelle[0] == 'C':
-            note_sep = note.split()
-            j = j+1 
-            if (information_finale[i][j][0] == '' or information_finale[i][j][0] == ' ') and i<3:
-                i = i+1
-                j = 1
-            for elt in note_sep:
-                if(elt != '-'):
-                    information_finale[i,j,5+continu]=elt
-                    continu = continu + 1
-            continu = 0
-        elif libelle[0] == 'E':
-            note_sep = note.split()
-            j = j+1
-            if(last == 'C' or last == 0):
-                i = i
-                j = j - 1
-            elif (information_finale[i][j][0] == '' or information_finale[i][j][0] == ' ') and i<3:
-                i = i+1
-                j = 1
-            for elt in note_sep:
-                if(elt != '-'):
-                    information_finale[i,j,13+examen] = elt
-                    examen = examen + 1
-            examen = 0
-        elif libelle[0] == 'P':
-            note_sep = note.split()
-            j = j+1
-            if(last == 'E' or last == 0):
-                i = i
-                j = j - 1
-            elif (information_finale[i][j][0] == '' or information_finale[i][j][0] == ' ') and i<3:
-                i = i+1
-                j = 1
-            for elt in note_sep:
-                if(elt != '-'):
-                    information_finale[i,j,24+projet] = elt
-                    projet = projet + 1
-            projet = 0
-        last = libelle[0]
-
-def afficherTableau():
-    for i in range(4):
-        for j in range(15):
-            for k in range(30):
-                if information_finale[i,j,k] != "":
-                    print(information_finale[i,j,k], sep=" ", end=" ")
-            print("\n")
-        print("\n\n\n\n")
-
+def calculerMoyenneTableau(tableau,module,matiere,compteur):
+    tab_moyenne = np.full((6,2), -1,float)
+    ligne = 0
+    pattern = r"^\d+(\,\d{1,2})?$"
+    bonus_moyenne = 0
+    suiteOuNon = 0
+    
+    #Charger les notes de controle CC
+    if tableau[module][matiere][0].find("Bonus") == -1:
+        
+        if re.match(pattern, tableau[module][matiere][compteur]) and re.match(pattern, tableau[module][matiere][compteur+2]):
+            tab_moyenne[ligne][0] = float(tableau[module][matiere][compteur].replace(",", "."))
+            tab_moyenne[ligne][1] = float(tableau[module][matiere][compteur+1].replace("(", "").replace(")", "").replace("%", "").strip())
+            suiteOuNon = 1
+            ligne += 1
+            compteur += 2
+            while(suiteOuNon == 1):
+                if re.match(pattern, tableau[module][matiere][compteur]):
+                    tab_moyenne[ligne][0] = float(tableau[module][matiere][compteur].replace(",", "."))
+                    if tableau[module][matiere][compteur+1] != '' :tab_moyenne[ligne][1] = float(tableau[module][matiere][compteur+1].replace("(", "").replace(")", "").replace("%", "").strip())
+                    compteur += 2
+                    ligne += 1
+                else :
+                    suiteOuNon = 0
+                    
+        elif re.match(pattern, tableau[module][matiere][compteur]) and not re.match(pattern, tableau[module][matiere][compteur+2]):
+            tab_moyenne[ligne][0] = float(tableau[module][matiere][compteur].replace(",", "."))
+            if tableau[module][matiere][compteur+1] != '' :tab_moyenne[ligne][1] = float(tableau[module][matiere][compteur+1].replace("(", "").replace(")", "").replace("%", "").strip())
+            suiteOuNon = 0
+            ligne += 1
+    else:
+        if tableau[module][matiere][compteur] != '' and tableau[module][matiere][compteur] != " ":
+            entreeTexte = tableau[module][matiere][compteur]
+            bonus_moyenne = (float(entreeTexte.replace(",", "."))-10)/10
+    
+    return bonus_moyenne,tab_moyenne
+    
+        
 
 def calculerMoyenneMatiere(tableau,module,matiere):
-    tab_moyenne_CC = np.full((6,2), -1,float)
-    tab_moyenne_EX = np.full((6,2), -1,float)
-    tab_moyenne_PR = np.full((6,2), -1,float)
-    pattern = r"^\d+(\,\d{1,2})?$"
+    bonus,tab_moyenne_CC= calculerMoyenneTableau(tableau, module, matiere, 5)
+    tab_moyenne_EX = calculerMoyenneTableau(tableau, module, matiere, 13)[1]
+    tab_moyenne_PR = calculerMoyenneTableau(tableau, module, matiere, 24)[1]
     ligne = 0
-    compteur_CC = 5
-    compteur_EX = 13
-    compteur_PR = 24
     coeff_CC = tableau[module][matiere][2] if tableau[module][matiere][2] != '' else 0
     coeff_EX = tableau[module][matiere][3] if tableau[module][matiere][3] != '' else 0
     coeff_PR = tableau[module][matiere][4] if tableau[module][matiere][4] != '' else 0
@@ -175,83 +193,7 @@ def calculerMoyenneMatiere(tableau,module,matiere):
     somme_Coef_PR = 0
     somme_Coef_EX = 0
     moyenne_Generale = 0
-    suiteOuNon = 0
-    bonus = 0
     
-    #Charger les notes de controle CC
-    if tableau[module][matiere][0].find("Bonus") == -1:
-        
-        if re.match(pattern, tableau[module][matiere][compteur_CC]) and re.match(pattern, tableau[module][matiere][compteur_CC+2]):
-            tab_moyenne_CC[ligne][0] = float(tableau[module][matiere][compteur_CC].replace(",", "."))
-            tab_moyenne_CC[ligne][1] = float(tableau[module][matiere][compteur_CC+1].replace("(", "").replace(")", "").replace("%", "").strip())
-            suiteOuNon = 1
-            ligne += 1
-            compteur_CC += 2
-            while(suiteOuNon == 1):
-                if re.match(pattern, tableau[module][matiere][compteur_CC]):
-                    tab_moyenne_CC[ligne][0] = float(tableau[module][matiere][compteur_CC].replace(",", "."))
-                    if tableau[module][matiere][compteur_CC+1] != '' :tab_moyenne_CC[ligne][1] = float(tableau[module][matiere][compteur_CC+1].replace("(", "").replace(")", "").replace("%", "").strip())
-                    compteur_CC += 2
-                    ligne += 1
-                else :
-                    suiteOuNon = 0
-                    
-        elif re.match(pattern, tableau[module][matiere][compteur_CC]) and not re.match(pattern, tableau[module][matiere][compteur_CC+2]):
-            tab_moyenne_CC[ligne][0] = float(tableau[module][matiere][compteur_CC].replace(",", "."))
-            if tableau[module][matiere][compteur_CC+1] != '' :tab_moyenne_CC[ligne][1] = float(tableau[module][matiere][compteur_CC+1].replace("(", "").replace(")", "").replace("%", "").strip())
-            suiteOuNon = 0
-            ligne += 1
-        
-        #Charger les notes d'EX
-        ligne = 0
-        if re.match(pattern, tableau[module][matiere][compteur_EX]) and re.match(pattern, tableau[module][matiere][compteur_EX+2]):
-            tab_moyenne_EX[ligne][0] = float(tableau[module][matiere][compteur_EX].replace(",", "."))
-            tab_moyenne_EX[ligne][1] = float(tableau[module][matiere][compteur_EX+1].replace("(", "").replace(")", "").replace("%", "").strip())
-            suiteOuNon = 1
-            ligne += 1
-            compteur_EX += 2
-            while(suiteOuNon == 1):
-                if re.match(pattern, tableau[module][matiere][compteur_EX]):
-                    tab_moyenne_EX[ligne][0] = float(tableau[module][matiere][compteur_EX].replace(",", "."))
-                    if tableau[module][matiere][compteur_EX+1] != '' :tab_moyenne_EX[ligne][1] = float(tableau[module][matiere][compteur_EX+1].replace("(", "").replace(")", "").replace("%", "").strip())
-                    compteur_EX += 2
-                    ligne += 1
-                else :
-                    suiteOuNon = 0
-            
-        elif re.match(pattern, tableau[module][matiere][compteur_EX]) and not re.match(pattern, tableau[module][matiere][compteur_EX+2]):
-            tab_moyenne_EX[ligne][0] = float(tableau[module][matiere][compteur_EX].replace(",", "."))
-            if tableau[module][matiere][compteur_EX+1] != '' : tab_moyenne_EX[ligne][1] = float(tableau[module][matiere][compteur_EX+1].replace("(", "").replace(")", "").replace("%", "").strip())
-            suiteOuNon = 0
-            ligne += 1
-    
-    
-        #charger les notes de projet
-        ligne = 0
-        if re.match(pattern, tableau[module][matiere][compteur_PR]) and re.match(pattern, tableau[module][matiere][compteur_PR+2]):
-            tab_moyenne_PR[ligne][0] = float(tableau[module][matiere][compteur_PR].replace(",", "."))
-            tab_moyenne_PR[ligne][1] = float(tableau[module][matiere][compteur_PR+1].replace("(", "").replace(")", "").replace("%", "").strip())
-            suiteOuNon = 1
-            ligne += 1
-            compteur_PR += 2
-            while(suiteOuNon == 1):
-                if re.match(pattern, tableau[module][matiere][compteur_PR]):
-                    tab_moyenne_PR[ligne][0] = float(tableau[module][matiere][compteur_PR].replace(",", "."))
-                    if tableau[module][matiere][compteur_PR+1] != '' :tab_moyenne_PR[ligne][1] = float(tableau[module][matiere][compteur_PR+1].replace("(", "").replace(")", "").replace("%", "").strip())
-                    compteur_PR += 2
-                    ligne += 1
-                else :
-                    suiteOuNon = 0
-            
-        elif re.match(pattern, tableau[module][matiere][compteur_PR]) and not re.match(pattern, tableau[module][matiere][compteur_PR+2]):
-            tab_moyenne_PR[ligne][0] = float(tableau[module][matiere][compteur_PR].replace(",", "."))
-            if tableau[module][matiere][compteur_PR+1] != '' :tab_moyenne_PR[ligne][1] = float(tableau[module][matiere][compteur_PR+1].replace("(", "").replace(")", "").replace("%", "").strip())
-            suiteOuNon = 0
-            ligne += 1
-    else:
-        if tableau[module][matiere][compteur_CC] != '' and tableau[module][matiere][compteur_CC] != " ":
-            entreeTexte = tableau[module][matiere][compteur_CC]
-            bonus = (float(entreeTexte.replace(",", "."))-10)/10
     
     for ligne in range (6):
         if tab_moyenne_CC[ligne][0] != -1:
@@ -307,7 +249,6 @@ def calculerMoyenneMatiere(tableau,module,matiere):
             
     return moyenne_Generale,bonus
     
-
 def calculerMoyenneModule(tableau, numeroModule):
     somme_Moyenne_module = 0
     somme_Coef_module = 0
@@ -357,21 +298,3 @@ def calculerMoyenneGenerale(tableau):
     else:
         moyenneGenerale = -1
     print('Moyenne Générale : ' + str(moyenneGenerale))
-    
-calculerMoyenneGenerale(information_finale)
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
